761ac44d2120d5f5ee737f70753f7f5785bcafc8
Harshit yadav <147736224+harshitneversettle@users.noreply.github.com>
2026-02-01 01:24:59 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   server: {
+//     host: "0.0.0.0",
+//     port: 5173,
+//     allowedHosts: [
+//       "ec2-16-171-16-206.eu-north-1.compute.amazonaws.com"
+//     ]
+//   }
+// });
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    allowedHosts: [
-      "ec2-16-171-16-206.eu-north-1.compute.amazonaws.com"
-    ]
-  }
+  plugins: [react()],
 });
