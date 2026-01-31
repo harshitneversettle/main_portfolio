f7f4edf8cc4592b8060e2e6ac54411f666edb60f
Harshit yadav <147736224+harshitneversettle@users.noreply.github.com>
2026-02-01 01:21:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: [
+      "ec2-16-171-16-206.eu-north-1.compute.amazonaws.com"
+    ]
+  }
 });
